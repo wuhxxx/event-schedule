c150ca6499bc1506ea660d65afaf99ae3591ca55
wuhxxx <wuhxxx@gmail.com>
2019-01-25 20:13:53 -0800

    update redux devtool extension compose

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -26,8 +26,7 @@ const middlewares = [thunk];
 const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__
     ? compose(
           applyMiddleware(...middlewares),
-          window.__REDUX_DEVTOOLS_EXTENSION__ &&
-              window.__REDUX_DEVTOOLS_EXTENSION__()
+          window.__REDUX_DEVTOOLS_EXTENSION__()
       )
     : compose(applyMiddleware(...middlewares));
 
